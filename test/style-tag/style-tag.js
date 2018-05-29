764d83130dc512f6524db65ef6fe28482a109e6e
tanem <tane.morgan@gmail.com>
2018-05-30 05:55:04 +1200

    Tidy up cors tests

--- test/style-tag/style-tag.js
+++
@@ -1,3 +0,0 @@
-const styleTag = `<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512"><defs><style></style></defs><text y="50" font-size="20" fill="#fff" display="block">PASS: The SVG style tag(s) were used properly.</text></svg>`
-
-export default styleTag
