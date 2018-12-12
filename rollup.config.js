44769d318363e902b3d7f34ef485b70e30d23676
Tane Morgan <tane.morgan@gmail.com>
2018-12-12 13:48:27 +1300

    Decrufting

--- rollup.config.js
+++ rollup.config.js
@@ -105,28 +105,3 @@ export default [
   getUmdConfig(UMD_DEV),
   getUmdConfig(UMD_PROD)
 ]
-
-// import babel from 'rollup-plugin-babel'
-// import { uglify } from 'rollup-plugin-uglify'
-
-// const devConfig = {
-//   input: 'src/index.js',
-//   output: {
-//     file: 'umd/svg-injector.js',
-//     format: 'umd',
-//     name: 'SVGInjector'
-//   },
-//   plugins: [babel()]
-// }
-
-// const prodConfig = {
-//   input: 'src/index.js',
-//   output: {
-//     file: 'umd/svg-injector.min.js',
-//     format: 'umd',
-//     name: 'SVGInjector'
-//   },
-//   plugins: [babel(), uglify()]
-// }
-
-// export default [devConfig, prodConfig]
