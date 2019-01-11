ca4caa7f936cb52944fb451a0552e5899165da70
Tane Morgan <tane.morgan@gmail.com>
2019-01-11 15:20:07 +1300

    Restore original comment

--- src/request-queue.js
+++ src/request-queue.js
@@ -10,6 +10,7 @@ export const queueRequest = (url, callback) => {
 
 export const processRequestQueue = url => {
   for (let i = 0, len = requestQueue[url].length; i < len; i++) {
+    // Make these calls async so we avoid blocking the page/renderer.
     ;(function(index) {
       setTimeout(function() {
         const cb = requestQueue[url][index]
