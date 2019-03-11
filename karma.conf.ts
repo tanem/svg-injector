3f7e608fdf30423dde11b5693c2a859b6fcd2d14
Tane Morgan <tane.morgan@gmail.com>
2019-03-12 10:06:40 +1300

    Switch to ESLint from TSLint

--- karma.conf.ts
+++ karma.conf.ts
@@ -1,4 +1,4 @@
-const path = require('path')
+import path from 'path'
 
 const PORT = 9876
 
--- rollup.config.js
+++ rollup.config.js
@@ -62,8 +62,8 @@ const getPlugins = bundleType => [
           sourcemap: true,
           output: { comments: false },
           compress: {
-            keep_infinity: true,
-            pure_getters: true
+            keep_infinity: true, // eslint-disable-line @typescript-eslint/camelcase
+            pure_getters: true // eslint-disable-line @typescript-eslint/camelcase
           },
           warnings: true,
           ecma: 5,
