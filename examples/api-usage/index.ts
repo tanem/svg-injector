f9e66f1a0e2e919d4d354872b9ec1b276f76b8c1
Tane Morgan <464864+tanem@users.noreply.github.com>
2019-12-17 06:54:05 +1300

    Refactor examples

---
+++ examples/api-usage/index.ts
@@ -0,0 +1,18 @@
+import { SVGInjector } from '@tanem/svg-injector'
+
+SVGInjector(document.getElementsByClassName('inject-me'), {
+  afterAll(elementsLoaded) {
+    console.log(`injected ${elementsLoaded} elements`)
+  },
+  afterEach(err, svg) {
+    if (err) {
+      throw err
+    }
+    console.log(`injected ${svg.outerHTML}`)
+  },
+  beforeEach(svg) {
+    svg.setAttribute('stroke', 'red')
+  },
+  evalScripts: 'once',
+  renumerateIRIElements: 'false'
+})
